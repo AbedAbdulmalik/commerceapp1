6765ff47f92df2f29fcce098e6a81ae320400253
AbedAbdulmalik <97865835+AbedAbdulmalik@users.noreply.github.com>
2024-08-17 22:29:39 -0400

    Update wsgi.py

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,24 +1,9 @@
-"""
-WSGI config for backend project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
-
 import os
 import sys
-
 from django.core.wsgi import get_wsgi_application
 
-# Print statements for debugging
-print("WSGI script is running...")
-print("Current working directory:", os.getcwd())
-print("System path:", sys.path)
-
-# Ensure that the backend directory is in the Python path
-sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'backend'))
+# Ensure the backend directory is in the Python path
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
