eed63f0627c5bd7a2c51c45cc746d90b9b8b6790
AbedAbdulmalik <97865835+AbedAbdulmalik@users.noreply.github.com>
2024-08-17 22:22:23 -0400

    Update wsgi.py

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -8,8 +8,17 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+# Print statements for debugging
+print("WSGI script is running...")
+print("Current working directory:", os.getcwd())
+print("System path:", sys.path)
+
+# Ensure that the backend directory is in the Python path
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'backend'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
