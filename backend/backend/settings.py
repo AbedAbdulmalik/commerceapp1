1d2e957729f0a31f18db08eb332084f32b08785f
AbedAbdulmalik <97865835+AbedAbdulmalik@users.noreply.github.com>
2024-08-17 22:56:21 -0400

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -68,6 +68,7 @@ ALLOWED_HOSTS = ['*']
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
